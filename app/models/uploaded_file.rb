88cc5ac8c7755416e7a283e249864eda7bc6b342
Eren Icel <erenicel@Erens-MacBook-Pro.local>
2013-03-04 21:17:57 +0000

    enable the console

--- app/models/uploaded_file.rb
+++ app/models/uploaded_file.rb
@@ -3,7 +3,6 @@ class UploadedFile < ActiveRecord::Base
   has_attachment :storage => :file_system,
                  :max_size => 2.megabytes,
                  :path_prefix => 'public/system/uploads',
-                 :thumbnails => { :small => '315x315' },
-                 :processor => 'Image_Science'
+                 :thumbnails => { :small => '315x315' }
   validates_as_attachment
 end
